10c5cf272f70c6d638f5a1b664871e1088859373
IdoGalil <33372318+IdoGalil@users.noreply.github.com>
2018-04-24 15:50:58 +0300

    Add files via upload

---
+++ test_fsm.v
@@ -0,0 +1,40 @@
+module test_fsm; 
+
+reg clock, reset, in;
+wire out;
+
+  my_fsm U0 ( 
+  .clock (clock),
+  .reset (reset),
+  .in    (in),
+  .out  (out) 
+  ); 
+   
+ 
+initial begin
+  clock = 1;
+  reset = 1;
+  in = 0;
+  $display("Starting simulation...");
+  #2;
+  reset = 0;
+  #2;
+  if (out !== 1'b0) $display("Failed"); 
+  in = 1;
+  #2;
+  if (out !== 1'b0) $display("Failed");
+  in = 0;  
+  #2;
+  if (out !== 1'b0) $display("Failed");
+  in = 1;
+  #2;
+  if (out !== 1'b1) $display("Failed");  
+  $display("Done...");
+end 
+  
+
+always begin
+   #1 clock = !clock;
+end
+   
+endmodule
